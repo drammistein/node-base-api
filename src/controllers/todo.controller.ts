90487305874a5416a345b0d2f89e975bb85b1678
Daniel Ramos <drammistein@hotmail.com>
2022-03-21 21:45:07 -0300

    Update a task

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -17,13 +17,36 @@ export const add = async (req: Request, res: Response) => {
         });
         // Retorno dos dados
         res.status(201).json({ item: newTodo });
+
     } else {
         res.json({ error: 'Dados não enviaddos.'});
     }
 }
 
-export const update = async () => {
-
+export const update = async (req: Request, res: Response) => {
+    let id: string  = req.params.id;
+    let todo = await Todo.findByPk(id);
+    if(todo) {
+        if(req.body.title) {
+            todo.title = req.body.title;
+        }        
+        if(req.body.done) { // Usar quando for Boolean
+            switch(req.body.done) {
+                case 'true':
+                case '1':
+                    todo.done = true;
+                    break;
+                case 'false':
+                case '0':
+                    todo.done = false;
+                    break
+            }
+        }
+        await todo.save();
+        res.json({ item: todo });
+    } else {
+        res.json({error: 'Item não encontrado.'});
+    }
 }
 
 export const remove = async () => {
